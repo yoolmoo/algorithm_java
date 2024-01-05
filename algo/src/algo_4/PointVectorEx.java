84049fc9fb695e18afc251345d48349c1423b064
yoolmoo <133771039+yoolmoo@users.noreply.github.com>
2024-01-05 10:55:04 +0900

    commit

---
+++ algo/src/algo_4/PointVectorEx.java
@@ -0,0 +1,35 @@
+package algo_4;
+import java.util.Vector;
+
+class  Point{
+	private int x, y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString() {
+		return "(" + x + "," + y + ")";
+	}
+}
+
+public class PointVectorEx {
+	public static void main(String[] args) {
+		//Point객체를 요소로만 가지는 벡터 생성
+		Vector<Point> v = new Vector<Point>();
+		
+		//3개의 Point 객체 삽입
+		v.add(new Point(2,3));
+		v.add(new Point(-5, 20));
+		v.add(new Point(30, -8));
+		
+		v.remove(1); // 인덱스 1의 Point(-5, 20) 객체 삭제
+		
+		//벡터에 있는 Point객체 모두 검색하여 출력
+		for(int i=0;i<v.size();i++) {
+			Point p = v.get(i); //벡터에서 i번째 Point객체 얻어내기
+			System.out.println(p);//p.toString()을 이용하여 객체 p출력
+			
+		}
+	}
+}
