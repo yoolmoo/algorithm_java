d5e7b16dcf09279aa2257afaf170828e80e692b8
yoolmoo <133771039+yoolmoo@users.noreply.github.com>
2024-01-15 10:33:27 +0900

    commit

---
+++ algo/src/algo_7/Node.java
@@ -0,0 +1,16 @@
+package algo_7;
+
+public class Node<E> {
+	public E item;
+	public Node<E>next;
+		
+	public Node(E newItem) {
+		item = newItem;
+		next = null;
+	}
+	public Node(E newItem, Node<E>nextNode) {
+		item=newItem;
+		next=nextNode;
+	}
+}
+
